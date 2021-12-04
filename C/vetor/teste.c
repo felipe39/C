c2f6121df7c744e1a5c45f2dae4837aeed421b19
Felipe falcão <felpssalvatore42@gmail.com>
2021-12-04 15:05:01 -0300

    Ler 100 números e mostrar o maior deles

---
+++ C/vetor/teste.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*Faça um programa que leia 100 (comando for é o recomendado pois o núemro é definido)
+números inteiros positivos e mostre o maior dele*/
+int main(){
+  int maiorNumero = 0, numero, i = 1;
+
+  do{
+    printf("Digite um número: ");
+    scanf("%d",&numero);
+    if(numero > maiorNumero)
+      maiorNumero = numero;
+
+    //printf("Numero lido: %d\n",numero);
+
+    i++;
+  }while(i<=2);
+
+  printf("Maior número lido: %d\n",maiorNumero);
+}
