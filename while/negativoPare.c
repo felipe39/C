45838b20e392543214b1adef91ce142dff7dba82
Felipe falcão <felpssalvatore42@gmail.com>
2021-12-04 19:44:06 -0300

    Reajustando

--- while/negativoPare.c
+++ while/negativoPare.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 /*Programa que ler números inteiros até que sejá digitado um número negativo*/
 int main(){
-  int numero, i = 0;
-
-    do {
+  int numero, quantidade;
+  numero = quantidade = 0;
+     while(numero >= 0){
 
       printf("Digite um número: ");
       scanf("%d",&numero);
       if(numero >= 0)
 
-      i++;
-    }while(numero >= 0);
-    printf("Quantidade de números lidos: %d \n",i);
+      quantidade++;
+    }
+    printf("Quantidade de números lidos: %d \n",quantidade);
 }
