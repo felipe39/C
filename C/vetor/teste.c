45829b6c66debbe7de968f9e405899ea0977acc1
Felipe falcão <felpssalvatore42@gmail.com>
2021-12-04 15:15:03 -0300

    Convertendo o comando do while para for (mais adequado nessa situação).

--- C/vetor/teste.c
+++ C/vetor/teste.c
@@ -2,18 +2,16 @@
 /*Faça um programa que leia 100 (comando for é o recomendado pois o núemro é definido)
 números inteiros positivos e mostre o maior dele*/
 int main(){
-  int maiorNumero = 0, numero, i = 1;
+  int maiorNumero = 0, numero, i;
 
-  do{
+  for(i = 1; i <= 3: i++){
     printf("Digite um número: ");
     scanf("%d",&numero);
     if(numero > maiorNumero)
+
       maiorNumero = numero;
 
-    //printf("Numero lido: %d\n",numero);
-
-    i++;
-  }while(i<=2);
+  }
 
   printf("Maior número lido: %d\n",maiorNumero);
 }
