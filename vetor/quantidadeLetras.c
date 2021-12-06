85c87c7eb2901c495d21f21c630387499575d358
Felipe falcão <felpssalvatore42@gmail.com>
2021-12-06 19:44:02 -0300

    Otimização no comentário

--- vetor/quantidadeLetras.c
+++ vetor/quantidadeLetras.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
-
+/*
+*Autor: Felipe falcão
+*
+*Data: 06/12/2021
+*
+******/
 int main(){
   const int tamanho = 5;//constante com um valor fixo
   int i, posicao, vet[tamanho];//5 posições para 5 letras
   char letra;
   char nome[255];
 
+  /*Vai inicializar todas as posições do vetor com zero*/
   for(i = 0; i <= 4; i++)
   {
     vet[i] = 0;
@@ -13,8 +19,12 @@ int main(){
   letra = getchar();
   //scanf("%c",&letra);
 
+    /*Vai tertar se letra é diferente de z para poder entrar*/
     while(letra != 'z')
     {
+      /*Comando caso, vai testar as possibilidades de letras que entrou
+      no comando enquanto, while. Caso sejá 'a' faça tal coisa, nesse Caso
+      vai ser incrementado certa posição do vetor.*/
       switch (letra)
       {
         case 'a':
@@ -33,9 +43,12 @@ int main(){
           vet[4]++;
           break;
       }
-      letra = getchar();
+      letra = getchar();//Vai digitar a letra até que sejá z
     }
     printf("Quantidade de cada vogal, em ordem: \n");
+    /*Vai percorrer o vetor, imprimindo cada posição das 5 declaradas
+    Essas posições contém a quantidade de letras incrementadas de declaradas
+    vogal digitada*/
     for(posicao = 0; posicao <= 4; posicao++)
     {
       printf("%d. ",vet[posicao]);
