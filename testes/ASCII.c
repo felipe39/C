5c53f680ed401fbd07663f25ec2c3c078b33498e
Felipe falcão <felpssalvatore42@gmail.com>
2021-12-07 20:53:06 -0300

    Mudanda a função genérica para uma especifica

---
+++ testes/ASCII.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+*Autor: Felipe Falcão
+*Data:  07/12/2021
+*
+*
+*Programa que mostra o ASCII do caractere
+******/
+int main(){
+  char ch;
+  printf("Introduza um caractere: ");
+  ch = getchar();
+  printf("O caractere '%c' tem o ASCII n° %d\n", ch, ch);
+}
