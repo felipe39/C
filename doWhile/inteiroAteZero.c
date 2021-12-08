c5dbe6b144cd05963c8a9613ebf72343aa41a0b6
Felipe falcão <felpssalvatore42@gmail.com>
2021-12-08 11:54:23 -0300

    Otimizações

--- doWhile/inteiroAteZero.c
+++ doWhile/inteiroAteZero.c
@@ -1,25 +1,19 @@
 #include<stdio.h>
-/*
-*Autor: Felipe Falcão
-*Data:  08/12/2021
-*
-*Programa que  lê números inteiros até que sejá zero. No final mostra
-*Os números positivos e negativos.
-*********/
+
 int main(){
   int numero, somaPositivo, somaNegativo;
   somaPositivo = somaNegativo = 0;
 
-    scanf("%d",&numero);
-    while(numero != 0)
-    {
+    do{
+
+      scanf("%d",&numero);
       if(numero > 0)
         somaPositivo = somaPositivo + numero;
       else
         somaNegativo = somaNegativo + numero;
 
-      scanf("%d",&numero);
-    }
+    }while(numero != 0);
+
     printf("Soma dos número positivos:  %d\n",somaPositivo);
     printf("SOma dos números negativos: %d\n",somaNegativo);
 }
